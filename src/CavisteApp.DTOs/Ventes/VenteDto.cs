0fdff831a1c732b8b6d53f6621ab5ade28b6bbfa
LucaRabbit <luca.ramampy@gmail.com>
2026-05-13 10:17:52 +0200

    chore(setup): ajouter projets api + wpf

---
+++ src/CavisteApp.DTOs/Ventes/VenteDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CavisteApp.DTOs.Ventes;
+
+public class VenteDto
+{
+    public int Id { get; set; }
+    public DateTime Date{ get; set; }
+    public decimal MontantTotal { get; set; }
+}
