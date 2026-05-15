7f2d16c0197abaff6baed5fb790992e9e7d7bff0
LucaRabbit <luca.ramampy@gmail.com>
2026-05-15 20:07:52 +0200

    fix(vin): changer Type en enum TypeVin

--- src/CavisteApp.DTOs/Vins/VinDto.cs
+++ src/CavisteApp.DTOs/Vins/VinDto.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.ComponentModel.DataAnnotations;
+using CavisteApp.Api.Enums;
 
 namespace CavisteApp.DTOs.Vins
 {
@@ -26,7 +27,7 @@ namespace CavisteApp.DTOs.Vins
         public int SeuilStockBas { get; set; }
 
         [Required]
-        public int Type { get; set; }
+        public TypeVin Type { get; set; }
 
         
 
