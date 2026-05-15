cb55642473c4858e785f2c53b1e6c71ae9b8bda8
LucaRabbit <luca.ramampy@gmail.com>
2026-05-15 22:41:14 +0200

    feat(vin): ajouter DataAnnotations aux Dtos

--- src/CavisteApp.DTOs/Vins/VinDto.cs
+++ src/CavisteApp.DTOs/Vins/VinDto.cs
@@ -7,30 +7,12 @@ namespace CavisteApp.DTOs.Vins
 {
     public class VinDto
     {
-        [Required]
         public int Id { get; set; }
-
-        [Required]
         public string Nom { get; set; } = string.Empty;
-
-        [Required]
         public DateTime CreatedDate { get; set; }
-
-        [Required]
         public decimal Prix { get; set; }
-
-        [Required]
         public int Stock { get; set; }
-
-        [Required]
         public int SeuilStockBas { get; set; }
-
-        [Required]
         public int Type { get; set; }
-
-        
-
-
-
     }
 }
