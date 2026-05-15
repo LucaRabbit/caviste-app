9e8a75d2bd732c4bc4267dae07811cf0196c29a0
LucaRabbit <luca.ramampy@gmail.com>
2026-05-15 22:11:55 +0200

    feat(vente): ajouter DataAnnotations aux Dtos

--- src/CavisteApp.DTOs/Ventes/CreerVenteDto.cs
+++ src/CavisteApp.DTOs/Ventes/CreerVenteDto.cs
@@ -1,7 +1,11 @@
-﻿// CréerVenteDto : DTO pour la création d'une vente
+﻿using System.ComponentModel.DataAnnotations;
+
+// CréerVenteDto : DTO pour la création d'une vente
 namespace CavisteApp.DTOs.Ventes;
 public class CreerVenteDto
 {
+    [Required]
     public int ClientId { get; set; }
+    [Required]
     public List<CreerLigneVenteDto> Lignes { get; set; } = new();
 }
