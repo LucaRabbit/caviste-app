874176d222f3e9959b776c36859a9ab9ed0b47e2
LucaRabbit <luca.ramampy@gmail.com>
2026-05-15 23:17:47 +0200

    feat(fournisseur): ajouter Dtos fournisseur avec DataAnnotations

---
+++ src/CavisteApp.DTOs/Fournisseurs/FournisseurDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CavisteApp.DTOs.Fournisseurs;
+
+public class FournisseurDto
+{
+    public int Id { get; set; }
+    public string Nom { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Telephone { get; set; } = string.Empty;
+    public int NumRue { get; set; }
+    public string NomRue { get; set; } = string.Empty;
+    public string CodePostal { get; set; } = string.Empty;
+    public string Ville { get; set; } = string.Empty;
+}
