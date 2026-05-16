e0dd902379f7d3e6c571335a9d52215cdf3c1a45
LucaRabbit <luca.ramampy@gmail.com>
2026-05-16 13:18:18 +0200

    api(wineapi): créer DTOs et Mapper pour l'api externe

---
+++ src/CavisteApp.Api/ExternalApi/IWineApiClient.cs
@@ -0,0 +1,9 @@
+﻿using System.Net;
+using System.Text.Json;
+using CavisteApp.Api.ExternalApi;
+
+public interface IWineApiClient
+{
+    Task<WineSearchResponse> SearchAsync(string query, int limit = 20, CancellationToken ct = default);
+    Task<WineDetailsDto?> GetDetailsAsync(string wineId, CancellationToken ct = default);
+}
