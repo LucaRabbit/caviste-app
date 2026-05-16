458c72ed07679f31ea5de46b3e24f4e7a768e3db
LucaRabbit <luca.ramampy@gmail.com>
2026-05-16 16:07:34 +0200

    api(mail): ajouter services email et mailtrap

---
+++ src/CavisteApp.Api/Services/Email/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿// CavisteApp.Api/Services/Email/IEmailService.cs
+namespace CavisteApp.Api.Services.Email;
+
+public interface IEmailService
+{
+    Task EnvoyerAsync(string sujet, string corpsHtml, CancellationToken ct = default);
+}
