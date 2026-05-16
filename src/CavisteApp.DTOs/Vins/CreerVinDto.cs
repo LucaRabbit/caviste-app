4fdf888b698f56ed03f8f4ca232d46aca364b9c0
LucaRabbit <luca.ramampy@gmail.com>
2026-05-16 10:55:31 +0200

    api(vin): modifier Dtos des vins pour la gestion du stock

--- src/CavisteApp.DTOs/Vins/CreerVinDto.cs
+++ src/CavisteApp.DTOs/Vins/CreerVinDto.cs
@@ -12,19 +12,19 @@ namespace CavisteApp.DTOs.Vins
         public string Nom { get; set; } = string.Empty;
 
         [Required]
+        [Range(0, 4, ErrorMessage = "Le type doit être compris entre 0 et 4")]
+        public int Type { get; set; }
+
+        [Required]
         [Range(0.01, double.MaxValue, ErrorMessage = "Le prix doit être supérieur à zéro.")]
         public decimal Prix { get; set; }
 
-        [Required]
-        [Range(0, int.MaxValue, ErrorMessage = "Le stock ne peut pas être négatif.")]
-        public int Stock { get; set; }
+        [Range(0, int.MaxValue)]
+        public int StockInitial { get; set; } = 0;
 
         [Required]
         [Range(0, int.MaxValue, ErrorMessage = "Le seuil de stock bas ne peut pas être négatif.")]
         public int SeuilStockBas { get; set; } = 5;
 
-        [Required]
-        [Range(0, 4, ErrorMessage = "Le type doit être compris entre 0 et 4")]
-        public int Type { get; set; }
     }
 }
