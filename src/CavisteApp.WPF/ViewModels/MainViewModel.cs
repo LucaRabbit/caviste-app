7e953ee688723e0d60a3da897f594298ef580dfb
LucaRabbit <luca.ramampy@gmail.com>
2026-05-17 20:58:00 +0200

    ui(setup): créer une ViewModel pour l'affichage prinicpal

---
+++ src/CavisteApp.WPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿// ViewModels/MainViewModel.cs
+using System.Windows.Input;
+using CavisteApp.WPF.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CavisteApp.WPF.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    private readonly SessionService _session;
+    private readonly IServiceProvider _services;
+
+    private ViewModelBase? _currentViewModel;
+
+    public MainViewModel(SessionService session, IServiceProvider services)
+    {
+        _session = session;
+        _services = services;
+
+        AfficherVinsCommand = new RelayNavCommand(
+            () => CurrentViewModel = _services.GetRequiredService<VinsViewModel>());
+
+        // Pour une future vue :
+        // AfficherClientsCommand = new RelayNavCommand(
+        //     () => CurrentViewModel = _services.GetRequiredService<ClientsViewModel>());
+    }
+
+    public ViewModelBase? CurrentViewModel
+    {
+        get => _currentViewModel;
+        set => SetProperty(ref _currentViewModel, value);
+    }
+
+    public string MessageBienvenue =>
+        _session.Utilisateur is null
+            ? string.Empty
+            : $"Bienvenue {_session.Utilisateur.Login} ({_session.Utilisateur.Role}) 👤";
+
+    public ICommand AfficherVinsCommand { get; }
+}
