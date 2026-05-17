7c4eaaa1047d177c3a1c7d6b47023322a2f409a4
LucaRabbit <luca.ramampy@gmail.com>
2026-05-17 12:35:57 +0200

    api(wineapi): ajouter controller pour l'import de vins depuis l'api externe

---
+++ src/CavisteApp.Api/Controllers/ImportController.cs
@@ -0,0 +1,13 @@
+﻿using CavisteApp.Api.Services.WineApi;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController, Route("api/[controller]")]
+public class ImportController : ControllerBase
+{
+    private readonly WineImportService _import;
+    public ImportController(WineImportService s) => _import = s;
+
+    [HttpPost("wineapi")]
+    public async Task<IActionResult> Importer([FromQuery] string q, [FromQuery] int limite = 20, CancellationToken ct = default)
+        => Ok(new { ajoutes = await _import.ImporterAsync(q, limite, ct) });
+}
