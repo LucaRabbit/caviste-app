e505c17cd7a1bf05d7c75c4f1a775289e95de804
LucaRabbit <luca.ramampy@gmail.com>
2026-05-17 21:01:23 +0200

    ui(setup): simplifier MainWindow.xaml.cs

--- src/CavisteApp.WPF/Views/MainWindow.xaml.cs
+++ src/CavisteApp.WPF/Views/MainWindow.xaml.cs
@@ -1,32 +1,24 @@
 ﻿using System.Windows;
 using CavisteApp.WPF.Services;
+using CavisteApp.WPF.ViewModels;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CavisteApp.WPF.Views;
 
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
 public partial class MainWindow : Window
 {
     private readonly SessionService _session;
 
-    public MainWindow(SessionService session)
+    public MainWindow(MainViewModel viewModel, SessionService session)
     {
         InitializeComponent();
+        DataContext = viewModel;
         _session = session;
-
-        if (_session.Utilisateur is not null)
-        {
-            LblBienvenue.Text = $"Bienvenue {_session.Utilisateur.Login} ({_session.Utilisateur.Role})";
-        }
     }
 
     private void BtnDeconnexion_Click(object sender, RoutedEventArgs e)
     {
         _session.Deconnecter();
-
-        // Réouvrir la fenêtre de login
         var loginWindow = App.Services.GetRequiredService<LoginWindow>();
         loginWindow.Show();
         Close();
