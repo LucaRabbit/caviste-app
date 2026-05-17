e6e5cc3173cfb1884c3771a80531843453f86834
LucaRabbit <luca.ramampy@gmail.com>
2026-05-17 21:02:55 +0200

    feat(ui): ajouter une vue pour afficher les vins

---
+++ src/CavisteApp.WPF/Views/VinsView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+using CavisteApp.WPF.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CavisteApp.WPF.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour VinsView.xaml
+    /// </summary>
+    public partial class VinsView : UserControl
+    {
+        public VinsView()
+        {
+            InitializeComponent();
+            DataContext = App.Services.GetRequiredService<VinsViewModel>();
+        }
+    }
+}
