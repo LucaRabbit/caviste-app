2258a19ca04c51c2f723df0f9250dd3aca270c7d
LucaRabbit <luca.ramampy@gmail.com>
2026-05-17 10:08:56 +0200

    chore(setup): deplacer Enums dans projet DTOs

---
+++ src/CavisteApp.DTOs/Enums/MotifRetraitStock.cs
@@ -0,0 +1,10 @@
+﻿namespace CavisteApp.DTOs.Enums;
+
+public enum MotifRetraitStock
+{
+    Perte,
+    Casse,
+    Vol,
+    Degustation,
+    Autre
+}
---
+++ src/CavisteApp.DTOs/Enums/StatutVente.cs
@@ -0,0 +1,8 @@
+﻿namespace CavisteApp.DTOs.Enums;
+
+public enum StatutVente
+{
+    Brouillon,
+    Validee,
+    Annulee
+}
