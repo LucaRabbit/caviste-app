4abe3f1eca8a16c8ae324ac6353774d63560d0d6
Daalion <leojacquemet@gmail.com>
2026-05-18 11:52:53 +0200

    Ajout QuestPdf Service : ajout de la génération du pdf

--- src/CavisteApp.Api/Migrations/20260518071758_InitialCreate.Designer.cs
+++ src/CavisteApp.Api/Migrations/20260518071758_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CavisteApp.Api.Migrations
 {
     [DbContext(typeof(CavisteDbContext))]
-    [Migration("20260517105414_InitialCreate")]
+    [Migration("20260518071758_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
